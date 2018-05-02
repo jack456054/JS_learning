c711855144ba23f38b59585a89ad8aaa6e4275cb
WenYu Lee <leewen@oregonstate.edu>
2018-05-01 18:24:20 -0700

    5/1

---
+++ javascript/learn-to-code.js
@@ -0,0 +1,85 @@
+//var name = "Yoyo";
+//var age = 26;
+//
+//var message = "Hi, my name is " + name + " and I am " + age + " years old!";
+
+//var firstName = "Yoyo";
+//var lastName = "Lee";
+//var dataOfBirth = "09-30-91";
+//var age = 26;
+//var profileImgUrl = '';
+//
+//var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + " birthday!";
+//            alert(message);
+//console.log(loginWelcomeMessage);
+//brackets doesn't have this command
+
+//var sum = 10 + 15;
+//var sub = 15 - 10;
+//var mul = 10 * 3;
+//var div = 10 / 3;
+//var mod = 10 % 3;
+//var msg = "10 / 3 = 3 with a remainder of " + mod;
+//var result = 10 * ((5 + 3) - 4) - 500
+//console.log(sum);
+//console.log(sub);
+//console.log(mul);
+//console.log(div);
+//console.log(msg);
+//console.log(result);
+
+//var myAccountBalanace = 300;
+//var nikeSBShoes = 799.23;
+//var coupon = 500;
+//
+//if (nikeSBShoes <= myAccountBalanace) {
+//    myAccountBalanace -= nikeSBShoes;
+//    console.log("We just bought some dope shoes!");
+//    console.log("Account Balance: " + myAccountBalanace);
+//}
+//else if (nikeSBShoes - coupon <= myAccountBalanace){
+//    myAccountBalanace -= nikeSBShoes - coupon;
+//    console.log("We just bought some dope shoes with a coupon!")
+//    console.log("Account Balance: " + myAccountBalanace);
+//    
+//}
+//else {
+//    console.log("you too broke ofr shoes bra!");
+//}
+
+//var age = 23;
+//var joesAge = "23";
+//
+//if (age !== joesAge){
+//    console.log("These a are not the same!")
+//}
+//else{
+//    console.log("One of these is like the other...")
+//}
+
+if(1 === 1 && 2 === 2 || "joe" === "joe"){
+    console.log("These are both true!")
+}
+
+if(true && true){
+    console.log("These are the same!")
+}
+
+if(1 === 3 || "joe" === "joe"){
+    console.log("One of these are true!")
+}
+
+var cat1 = 5;
+var cat2 = 10;
+var cat3 = 1;
+var cat3DisabledHandicap = true;
+
+if (cat1 > cat2 && cat1 > cat3 && !cat3DisabledHandicap){
+    console.log("Cat1 is the cutest")
+}
+else if (cat2 > cat1 && cat2 > cat3 && !cat3DisabledHandicap){
+    console.log("Cat2 is the cutest")
+}
+else if (cat3 > cat1 && cat3 > cat2 || disabledHandicap){
+    console.log("Cat3 is the cutest")
+}
