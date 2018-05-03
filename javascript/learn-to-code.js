582b5484d322c7ac75b7f16807fd025a77e2e1ef
WenYu Lee <leewen@oregonstate.edu>
2018-05-02 22:08:39 -0700

    5/2

--- javascript/learn-to-code.js
+++ javascript/learn-to-code.js
@@ -57,29 +57,48 @@
 //    console.log("One of these is like the other...")
 //}
 
-if(1 === 1 && 2 === 2 || "joe" === "joe"){
-    console.log("These are both true!")
+//if(1 === 1 && 2 === 2 || "joe" === "joe"){
+//    console.log("These are both true!")
+//}
+//
+//if(true && true){
+//    console.log("These are the same!")
+//}
+//
+//if(1 === 3 || "joe" === "joe"){
+//    console.log("One of these are true!")
+//}
+//
+//var cat1 = 5;
+//var cat2 = 10;
+//var cat3 = 1;
+//var cat3DisabledHandicap = true;
+//
+//if (cat1 > cat2 && cat1 > cat3 && !cat3DisabledHandicap){
+//    console.log("Cat1 is the cutest")
+//}
+//else if (cat2 > cat1 && cat2 > cat3 && !cat3DisabledHandicap){
+//    console.log("Cat2 is the cutest")
+//}
+//else if (cat3 > cat1 && cat3 > cat2 || disabledHandicap){
+//    console.log("Cat3 is the cutest")
+//}
+
+var balance = [50, 1000, -300]
+
+var person = ["John", 12, "USA", true]
+// Don't do this
+
+var students = ["Timmy", "Janessa", "Arun"]
+
+var list = []
+
+list.push(students[0])
+
+var index = list.indexOf("Timmy")
+
+if (index > -1){
+    list.splice(index, 1)
 }
 
-if(true && true){
-    console.log("These are the same!")
-}
-
-if(1 === 3 || "joe" === "joe"){
-    console.log("One of these are true!")
-}
-
-var cat1 = 5;
-var cat2 = 10;
-var cat3 = 1;
-var cat3DisabledHandicap = true;
-
-if (cat1 > cat2 && cat1 > cat3 && !cat3DisabledHandicap){
-    console.log("Cat1 is the cutest")
-}
-else if (cat2 > cat1 && cat2 > cat3 && !cat3DisabledHandicap){
-    console.log("Cat2 is the cutest")
-}
-else if (cat3 > cat1 && cat3 > cat2 || disabledHandicap){
-    console.log("Cat3 is the cutest")
-}
+console.log(list)
